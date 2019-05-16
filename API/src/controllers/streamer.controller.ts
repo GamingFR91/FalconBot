a48d4c0fc57c6e1c92c347394a8cf27169bec5a9
Kalivins <kevillard@hotmail.fr>
2019-05-16 02:15:27 +0200

    updating repository and streamer controller

--- API/src/controllers/streamer.controller.ts
+++ API/src/controllers/streamer.controller.ts
@@ -1,10 +1,39 @@
 // Uncomment these imports to begin using these cool features!
+import {
+  Count,
+  CountSchema,
+  Filter,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getFilterSchemaFor,
+  getWhereSchemaFor,
+  patch,
+  put,
+  del,
+  requestBody
+} from '@loopback/rest';
 
-// import {inject} from '@loopback/context';
-import {get} from '@loopback/rest';
+import { Server } from '../models';
+import { ServerRepository } from '../repositories';
 export class StreamerController {
-  @get('/streamer')
-  streamer(): string {
-    return 'Hello world!';
+  constructor(
+    @repository(ServerRepository)
+    public serverRepository: ServerRepository,
+  ) { }
+  @get('/streamers/{id}', {
+    responses: {
+      '200': {
+        description: 'get streamers by Server id',
+        content: { 'application/json': {} },
+      },
+    },
+  })
+  async getAllStreamers(@param.path.number('id') id: number) {
+    return await this.serverRepository.findStreamersByServerId(id);
   }
 }
--- API/src/repositories/server.repository.ts
+++ API/src/repositories/server.repository.ts
@@ -1,15 +1,23 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Server} from '../models';
-import {DbDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import { DefaultCrudRepository } from '@loopback/repository';
+import { Server } from '../models';
+import { DbDataSource } from '../datasources';
+import { inject } from '@loopback/core';
 
 export class ServerRepository extends DefaultCrudRepository<
   Server,
   typeof Server.prototype.id
-> {
+  > {
   constructor(
     @inject('datasources.db') dataSource: DbDataSource,
   ) {
     super(Server, dataSource);
+  }
+  public findStreamersByServerId(id: number) {
+    let self = this;
+    return new Promise<any>(async (resolve, reject) => {
+      let server = await self.findById(id);
+      let streamers = server.twitchChannels;
+      resolve(streamers);
+    });
   }
 }
